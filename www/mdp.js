2c0cedcbf383f5a8caaa4eac0dcb7d73df22bbe0
Cédric Derue <cedric.derue@gmail.com>
2016-03-29 20:54:05 +0200

    Update mdp.js

--- www/mdp.js
+++ www/mdp.js
@@ -3,5 +3,8 @@
 module.exports = {
     authenticateWithState: function (clientId, redirectUri, state, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Mdp", "authenticateWithState", [clientId, redirectUri, state]);
+    },
+    test: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "Mdp", "test", [name]);
     }
 };
